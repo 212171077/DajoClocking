1784d56f9e5d5ac168b120cbe8ba9951b604080b
unknown <venussibiya@gmail.com>
2017-08-29 08:30:00 +0200

    first commit

---
+++ DajoChicking/DajoChicking/Clocking.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DajoChicking
+{
+    public partial class Clocking : ContentPage
+    {
+        String Date = "2017/08/28";
+        String Time = "13:26:00:00";
+        public Clocking()
+        {
+            InitializeComponent();
+            //Setting image to the image view based on 
+            //A specific platform
+            profileImage.Source = Device.OnPlatform(
+            iOS: ImageSource.FromResource("DajoChicking.profile.png"),
+            Android: ImageSource.FromResource("DajoChicking.profile.png"),
+            WinPhone: ImageSource.FromResource("DajoChicking.profile.png"));
+
+
+        }
+
+        public void clock()
+        {
+
+        }
+    }
+}
---
+++ DajoChicking/DajoChicking/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DajoChicking
+{
+    public partial class MainPage : ContentPage
+    {
+        String username;
+        String password;
+        public MainPage()
+        {
+            InitializeComponent();
+            //Setting image to the image view based on 
+            //A specific platform
+            image.Source = Device.OnPlatform(
+            iOS: ImageSource.FromResource("DajoChicking.logo.png"),
+            Android: ImageSource.FromResource("DajoChicking.logo.png"),
+            WinPhone: ImageSource.FromResource("DajoChicking.logo.png"));
+        }
+
+        private async void login(object sender, EventArgs e)
+        {
+            password = txtPassword.Text;
+            username = txtUsername.Text;
+
+            if (validateUser(username, password))
+            {
+                /*this.DisplayAlert(
+                    "Dial a Number",
+                    "Login Successful...!!",
+                    "Ok",
+                    "Cancel");*/
+               
+                await Navigation.PushModalAsync(new Clocking(), false);
+                
+            }
+            else
+            {
+                this.DisplayAlert(
+                    "Dial a Number",
+                    "Login Failed...!!",
+                    "Ok",
+                    "Cancel");
+            }
+           
+        }
+
+        public Boolean validateUser(String username,String password)
+        {
+            Boolean isValidateUser = true;
+
+            return isValidateUser;
+
+        }
+    }
+}
